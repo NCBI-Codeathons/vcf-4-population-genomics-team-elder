7c19bb609df6741d9e9563bd7944964b6e18f017
Jesse Elder <61200792+elder099@users.noreply.github.com>
2023-08-02 15:57:35 -0500

    Code to divide data into Waves

---
+++ BQ.1Wave_VCF.sql
@@ -0,0 +1,14 @@
+select * from (
+select * from (
+select (cast(g_ad_2 as double)/cast(dp as double)) as maf,* from "ncbi-vcf-codeathon-rc-db1"."annotated_variations"
+)
+where maf < .5 and g_ad_2 >=20
+) as t1
+LEFT JOIN (
+select * from (
+select "acc",array_join("collection_date_sam",'') as doc from "ncbi-vcf-codeathon-rc-db1"."metadata"
+)
+WHERE doc >= '2022-06-20' AND doc < '2022-11-21'
+) as t2
+ON t1."run" = t2."acc"
+ORDER BY doc DESC
