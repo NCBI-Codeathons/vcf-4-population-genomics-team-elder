4522c5c44e5c8b963cf0a6bd88d37a6f621cd5e8
Jesse Elder <61200792+elder099@users.noreply.github.com>
2023-08-04 13:30:27 -0500

    Add Covid Wave SQL queries
    Queries aggregate to find % of samples with alleles in population across different waves. Also import mean allele frequency at each position across the waves.

---
+++ Covid_Wave_Code/BQ.1_Aggregate_Mean.sql
@@ -0,0 +1,43 @@
+select *,
+--frequency/total sample count
+freq_count/cast((select count(DISTINCT t3.run) from (
+select * from (
+select * from (
+select (cast(g_ad_2 as double)/cast(dp as double)) as maf,* from "ncbi-vcf-codeathon-rc-db1"."annotated_variations"
+)
+where maf < .5 and g_ad_2 >=20
+) as t1
+--Now join
+JOIN (
+select * from (
+select "acc",array_join("collection_date_sam",'') as doc from "ncbi-vcf-codeathon-rc-db1"."metadata"
+)
+WHERE doc >= '2022-11-21' AND doc < '2023-01-16'
+) as t2
+ON t1."run" = t2."acc"
+) as t3
+) as double) as pop_freq
+--total sample count
+from
+(
+select t3.pos,count(t3.pos) as freq_count,avg(maf) as maf_mean  from
+(
+select * from (
+select * from (
+select (cast(g_ad_2 as double)/cast(dp as double)) as maf,* from "ncbi-vcf-codeathon-rc-db1"."annotated_variations"
+)
+where maf < .5 and g_ad_2 >=20
+) as t1
+JOIN (
+select * from (
+select "acc",array_join("collection_date_sam",'') as doc from "ncbi-vcf-codeathon-rc-db1"."metadata"
+)
+WHERE doc >= '2022-11-21' AND doc < '2023-01-16'
+) as t2
+ON t1."run" = t2."acc"
+ORDER BY doc DESC
+--LIMIT 300
+) as t3
+GROUP BY t3.pos
+ORDER BY count(t3.pos) DESC
+)
