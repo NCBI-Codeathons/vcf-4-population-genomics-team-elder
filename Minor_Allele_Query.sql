930649946e5f115900c94ab28f307c26159ab85f
Jesse Elder <61200792+elder099@users.noreply.github.com>
2023-08-04 13:35:40 -0500

    Delete Minor_Allele_Query.sql

--- Minor_Allele_Query.sql
+++
@@ -1,8 +0,0 @@
-###Search the NCBI VCF file and identify alleles that have less than .5 frequency and read depth is greater than or equal to 20
-##joins metadata and VCF file on SRA accession 
-select (cast(g_ad_2 as double)/cast(dp as double)) "maf"
-,"ncbi-vcf-codeathon-rc-db1"."metadata"."collection_date_sam"
-, "ncbi-vcf-codeathon-rc-db1"."annotated_variations".*
-from "ncbi-vcf-codeathon-rc-db1"."annotated_variations" join "ncbi-vcf-codeathon-rc-db1"."metadata"
- on "metadata"."acc"="annotated_variations"."run"
-where (cast(g_ad_2 as double)/cast(dp as double)) > .5 
